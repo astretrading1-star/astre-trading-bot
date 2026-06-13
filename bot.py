0d831f45e3d5f5afed12446099d4121ca6b6918f
astretrading1-star <astretrading1@gmail.com>
2026-06-13 17:21:27 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,18 +6,20 @@ TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [InlineKeyboardButton("⭐ Rejoindre le VIP", url="https://t.me/+8DK58OC8UJVkZDFk")],
+        [InlineKeyboardButton("⭐ Rejoindre le Groupe VIP Payant", url="https://t.me/+8DK58OC8UJVkZDFk")],
         [InlineKeyboardButton("👥 Groupe Gratuit", url="https://t.me/astretrading")],
         [InlineKeyboardButton("📞 Support", url="https://t.me/Astre971")]
     ]
 
     await update.message.reply_text(
-    "🔥 BIENVENUE CHEZ ASTRE TRADING 🔥\n\n"
+    "🔥 BIENVENUE {user_name} CHEZ ASTRE TRADING 🔥\n\n"
     "📈 Signaux Forex Premium\n"
     "₿ Opportunités Crypto\n"
     "📊 Analyses de marché\n"
     "🎓 Formation Trading\n"
     "⭐ Communauté VIP\n\n"
+    "Notre objectif est de vous aider à developper vos compétences et à trader avec discipline.\n\n"
+    "🚀 Bon trading et bienvenue dans la communauté Astre Trading\n\n"
     "👇 Choisissez une option ci-dessous :",
     reply_markup=InlineKeyboardMarkup(keyboard)
 )
